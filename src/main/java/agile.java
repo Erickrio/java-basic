7790f16104ac9cac237f06d4fa4e735836f1b6cf
Erick Costa Monteiro <erick.monteiro@afya.com.br>
2024-01-10 17:52:31 -0300

    List contacts on the page

---
+++ src/main/java/agile.java
@@ -0,0 +1,23 @@
+Sobre a vaga
+Location : BAL - GASPAR - CENTRAL
+
+
+City : Gaspar
+
+
+State : Santa Catarina (BR-SC)
+
+
+Country : Brazil (BR)
+
+
+Requisition Number : 26937
+
+
+Bunge has an exciting opportunity available for a Back End Developer. In this role you will be part of a global team working on challenging, meaningful projects impacting core business activities.
+
+
+Since 1818, Bunge has been connecting farmers to consumers to deliver essential food, feed, and fuel to the world. Looking to the future, our ambition is to continuously reinvent ourselves, leveraging data to be at the forefront of analytics, technology and talent to accomplish our purpose in a better, faster and simpler way. Bunge is committed to operating and thriving in the digital world – creating world class agile teams where teammates are empowered and encouraged to collaborate and test and learn to succeed.
+
+
+At Bunge, people don’t just come here to work, they come here to grow – solving challenges that directly impact the world with a diverse team of thinkers and doers. Bunge offers a strong compensation and benefits 
--- src/main/java/com/algaworks/ContatosControle.java
+++ src/main/java/com/algaworks/ContatosControle.java
@@ -1,13 +1,38 @@
 package com.algaworks;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ContatosControle {	
 	
+	//constante static
+	private static final ArrayList<Contato> LISTA_CONTATOS = new ArrayList<Contato>();
+	
+	static { //atalho ctrl + alt seta baixo ;)
+		LISTA_CONTATOS.add(new Contato("1", "Segovinha", "+55768-78989"));
+		LISTA_CONTATOS.add(new Contato("2", "Nilton Santos", "+55768-78989"));
+		LISTA_CONTATOS.add(new Contato("3", "Garrincha", "+55768-78989"));
+		LISTA_CONTATOS.add(new Contato("4", "Pelé", "+55768-78989"));
+		LISTA_CONTATOS.add(new Contato("5", "Zagallo", "+55768-78989"));
+		LISTA_CONTATOS.add(new Contato("6", "Tquinho", "+55768-78989"));
+	}
+	
+	
+//	private static final Contato[]LISTA_CONTATO_2 = new Contato[10];
+	
 	@GetMapping("/")
 	public String index() {
 		return "index";
+	}
+	
+	@GetMapping("/contatos")
+	public ModelAndView listar() {
+		ModelAndView modelAndView = new ModelAndView("listar");
+		modelAndView.addObject("contatos",LISTA_CONTATOS);
+		return modelAndView;
 	}
 }
